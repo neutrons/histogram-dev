ccfb9593458b9f5bdb1f9a4fc449ad0c06f6489b
Jiao Lin <linjiao@caltech.edu>
2007-10-18 00:52:33 +0000

    branch "ARCSeventdata":   histogram/applications/PlotHist.py: added support for histogram in h5 file
    git-svn-id: svn://danse.us/histogram/branches/ARCS-eventmodedata-histogrammer/histogram@1333 566a22ba-31b3-4a2e-9087-23d4df278cd3

--- applications/PlotHist.py
+++ applications/PlotHist.py
@@ -1,40 +1,53 @@
 #!/usr/bin/env python
 
 
-def plotPklFile( filename, min = None, max = None ):
-    from histogram.hpickle import load
-    h = load( filename )
+def plotHist( h, min = None, max = None ):
     from histogram.plotter import defaultPlotter
     defaultPlotter.interactive( 0 )
     defaultPlotter.plot( h, min = min, max = max)
     return
 
 
-usage = """
-PlotHist.py <histogram pickle file name>
-PlotHist.py <histogram pickle file name> <min> <max>
-"""
+def plotPklFile( filename, min = None, max = None ):
+    from histogram.hpickle import load
+    h = load( filename )
+    plotHist( h )
+    return
+
+
+def plotH5File( h5filename, pathinh5file, min = None, max = None ):
+    from histogram.hdf import load
+    h = load( h5filename, pathinh5file )
+    plotHist( h )
+    return
+
 
 
 def main():
-    import sys
-    argc = len(sys.argv)
-    if argc > 4 or argc < 2: raise usage
+    from optparse import OptionParser
+    usage = "usage: %prog [options] histogram-data-file [args]"
+    parser = OptionParser(usage)
+    parser.add_option("", "--min", dest="min", default = None,
+                      type = "float", help="minimum")
+    parser.add_option("", "--max", dest="max", default = None,
+                      type = "float", help="maximum")
 
-    min = max = None
+    (options, args) = parser.parse_args()
+    if not len(args) in [1,2]:
+        parser.error("incorrect number of arguments")
+        raise "should not reach here"
+
+    filename = args[0]
+    if filename.endswith( 'h5' ):
+        msg = "path to the histogram inside the h5 file '%s' is needed\n\n" % (
+            filename, )
+        msg += "  PlotHist.py --min=<min> --max=<max> histograms.h5 /path/to/histogram/in/h5file"
+        if len(args) != 2: parser.error( msg )
+
+        plotH5File( filename, args[1], min = min, max = max )
+        return
     
-    if argc == 2: 
-        fn = sys.argv[1]
-    elif argc == 3: 
-        fn = sys.argv[1]
-        min = float(sys.argv[2])
-    elif argc == 4: 
-        fn = sys.argv[1]
-        min = float(sys.argv[2])
-        max = float(sys.argv[3])
-    else: raise usage
-    
-    plotPklFile( fn, min = min, max = max )
+    plotPklFile( filename, min = min, max = max )
     return
 
 
