f58c336e9ecb66b687af37c99e844dd1eaa1ec01
Patrou, Maria <patroum@ornl.gov>
2025-02-18 10:54:04 -0500

    build cleanup

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,21 +4,19 @@
 mkdir -p $PREFIX/include/histogram
 cp -r lib/* $PREFIX/include/histogram/
 
-#shared library
-mkdir build_files/
-cp -r lib/* build_files/
-cd build_files
+#shared library for linux
+cd lib/
 src_files=$(ls *.cc)
 
 mkdir -p $PREFIX/lib
 g++ -c -fPIC $src_files
 g++ -shared -o $PREFIX/lib/libhistogram.so $(ls *.o)
-cd ../
+cd ..
 
 #Python files
 mkdir -p $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram
 cp -r src/histogram/* $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram/
 
-#Executable
+#Executable plothist
 mkdir -p $PREFIX/bin
 cp -r bin/plothist $PREFIX/bin/
