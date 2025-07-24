47c20dcb31dd7012a31816fda99466bfb63bcb2a
Patrou, Maria <patroum@ornl.gov>
2025-07-24 11:23:02 -0400

    rattler-build for importing pyproject toml varibles in recipe yaml

--- build.sh
+++ build.sh
@@ -2,22 +2,12 @@
 echo "HERERERERERERERRRE \n\n\n\n\n"
 #lib folder
 mkdir -p $PREFIX/include/histogram
-cp -r lib/* $PREFIX/include/histogram/
-
-# in case a shared library is needed
-# shared library for linux
-# cd lib/
-# src_files=$(ls *.cc)
-
-# mkdir -p $PREFIX/lib
-# ${CXX} -c -fPIC $src_files
-# ${CXX} -shared -o $PREFIX/lib/libhistogram.so $(ls *.o)
-# cd ..
+cp -r histogram-dev/lib/* $PREFIX/include/histogram/
 
 #Python files
 mkdir -p $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram
-cp -r src/histogram/* $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram/
+cp -r histogram-dev/src/histogram/* $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram/
 
 #Executable plothist
 mkdir -p $PREFIX/bin
-cp -r bin/plothist $PREFIX/bin/
+cp -r histogram-dev/bin/plothist $PREFIX/bin/
