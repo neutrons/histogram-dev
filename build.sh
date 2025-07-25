a6fcf0c03d90b088e751bddf74ade8819d682f58
Patrou, Maria <patroum@ornl.gov>
2025-07-25 12:55:27 -0400

    conda build with lib, bin, and src folders with custom build script

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-echo "HERERERERERERERRRE \n\n\n\n\n"
+echo "-------------  BUILD SCRIPT  -------------"
+python -m pip install . --no-deps --ignore-installed
+
 #lib folder
 mkdir -p $PREFIX/include/histogram
-cp -r histogram-dev/lib/* $PREFIX/include/histogram/
+cp -r lib/* $PREFIX/include/histogram/
 
 #Python files
 mkdir -p $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram
-cp -r histogram-dev/src/histogram/* $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram/
+ls src/histogram/
+cp -r src/histogram/* $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram/
 
 #Executable plothist
 mkdir -p $PREFIX/bin
-cp -r histogram-dev/bin/plothist $PREFIX/bin/
+cp -r bin/plothist $PREFIX/bin/
