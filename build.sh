f7b8adafcd0a00c352140e5f53c0d3cf7baf8518
Pete Peterson <peterfpeterson@gmail.com>
2025-08-15 15:16:35 -0400

    Clean up dependencies (#14)
    * Reduce dependencies
    * Turn up log level for pixi run conda-build

--- build.sh
+++ build.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 echo "-------------  BUILD SCRIPT  -------------"
+
+# write out the _version.py file
 python -m pip install . --no-deps --ignore-installed
-
-# in case a shared library is needed
-# shared library for linux
-# cd lib/
-# src_files=$(ls *.cc)
-
-# mkdir -p $PREFIX/lib
-# ${CXX} -c -fPIC $src_files
-# ${CXX} -shared -o $PREFIX/lib/libhistogram.so $(ls *.o)
-# cd ..
 
 #lib folder
 mkdir -p $PREFIX/include/histogram
 cp -r lib/* $PREFIX/include/histogram/
 
 #Python files
-mkdir -p $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram
-ls src/histogram/
-cp -r src/histogram/* $PREFIX/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')/site-packages/histogram/
+mkdir -p $PREFIX/lib/python/site-packages/histogram
+cp -r src/histogram/* $PREFIX/lib/python/site-packages/histogram/
 
 #Executable plothist
 mkdir -p $PREFIX/bin
